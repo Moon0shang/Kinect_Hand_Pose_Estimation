ab2a0df534a15c566173f562c84e019fb4402e1d
X <moon0shang@gmail.com>
2019-02-25 14:48:32 +0800

    delete unnecessary parts

--- Hand_segmentation.py
+++ Hand_segmentation.py
@@ -16,8 +16,7 @@ def convt_gray(depth):
         gray picture after filter;
         hand threshold
     """
-    # constrain the depth in 1~4096(255*16),
-    # and reshape as the picture size
+    # constrain the depth in 1~4096(255*16), and reshape as the picture size
     depth = depth.clip(1, 4096).reshape(424, 512)
     original_img = np.uint8(depth.copy() / 16.)
 
@@ -32,7 +31,7 @@ def convt_gray(depth):
     return depth, original_img, filt_img, threshod
 
 
-def creat_mask(gray_image, threshod, hands_num, flags=None, nums=0):
+def creat_mask(gray_image, threshod, hands_num):
     """
     create the mask to segment hands
 
@@ -40,12 +39,6 @@ def creat_mask(gray_image, threshod, hands_num, flags=None, nums=0):
         gray_image (ndarray): the gray image after filtling
         threshod (int): the hand threshold
         hands_num (int): the number of hands, can only be choosed from {1,2}
-        flags (int, optional): Defaults to None. the flag decide the save/show operate, 
-            0: only show;
-            1: only save;
-            2: save and show;
-            others: pass
-        nums (int, optional): Defaults to 0. the frame order number
 
     Returns:
         mask(ndarray): the mask created
@@ -58,7 +51,8 @@ def creat_mask(gray_image, threshod, hands_num, flags=None, nums=0):
     # find contours and select the one/two with largest Area which stand for the hands
     (contours, _) = cv2.findContours(
         bin_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    hand_contour = sorted(contours, key=cv2.contourArea, reverse=True)[:2]
+    hand_contour = sorted(contours, key=cv2.contourArea,
+                          reverse=True)[:hands_num]
 
     darks = np.zeros(bin_image.shape, dtype=np.uint8)
 
@@ -73,16 +67,6 @@ def creat_mask(gray_image, threshod, hands_num, flags=None, nums=0):
 
         # fill the dark image with hands part
         mask[i] = cv2.fillPoly(darks, hand, 255)
-
-        # whether to show or save the image
-        # if flags == 0:
-        #     save_show(mask[i], describe='mask')
-        # elif flags == 1:
-        #     save_show(mask[i], save_name='./mask/%d' % nums)
-        # elif flags == 2:
-        #     save_show(mask[i], describe='mask', save_name='./mask/%d' % nums)
-        # else:
-        #     pass
 
     return mask, p_max, p_min
 
@@ -120,43 +104,7 @@ def hand_segment(depth, filt_img, threshod, hands_num=1):
             seg_image.pop(1)
             break
 
-        # centerX = (p_max[i][0] + p_min[i][0]) // 2
-        # centerY = (p_max[i][1] + p_min[i][1]) // 2
-
-        # # if two hands were connected, then only segment one part
-        # box_y = 55
-        # if p_max[i][1] - p_min[i][1] < 120:
-        #     box_x = 55
-        #     seg_image[i] = depth_image[centerY - box_y: centerY + box_y,
-        #                                centerX - box_x: centerX + box_x]
-        # else:
-        #     box_x = 90
-        #     seg_image[i] = depth_image[centerY - box_y: centerY + box_y,
-        #                                centerX - box_x: centerX + box_x]
-        #     break
-
     return seg_image
-
-
-# def save_show(image, describe=None,  save_name=None):
-#     """
-#     show or/and save the image
-
-#     Args:
-#         image (ndarray): image to show
-#         describe (string, optional): Defaults to None. the description of image
-#         save_name (string, optional): Defaults to None. the path and filename to save current image
-#     """
-
-#     if save_name == None:
-#         cv2.imshow('%s' % describe, image)
-#         cv2.waitKey(0)
-#     else:
-#         cv2.imwrite('%s.jpg' % save_name, image)
-
-#         if describe != None:
-#             cv2.imshow('%s' % describe, image)
-#             cv2.waitKey(0)
 
 
 if __name__ == "__main__":
